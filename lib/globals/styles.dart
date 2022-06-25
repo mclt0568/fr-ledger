f07e9deca3af204b27d0b03d55dae773b21bd6b8
frankium <frankium>
2022-06-26 01:39:17 +0800

    add some basic styles

---
+++ lib/globals/styles.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/widgets.dart';
+
+const BoxShadow slightElevated = BoxShadow(
+    offset: Offset(0, 1.5),
+    blurRadius: 2,
+    spreadRadius: 1,
+    color: Color(0x88c5c5c5));
+
+// Colors
+const Color headerColor = Color(0xFF000000);
+const Color paragraphColorDark = Color(0xFF5B5B5B);
+const Color paragraphColorLight = Color(0xFF868686);
+const Color accent = Color(0xFF00826A);
+const Color acdentLighter = Color(0xFFC2F2EA);
+const Color accentDarker = Color(0xFF006351);
+const Color grayBackground = Color(0xFFF3F3F3);
+const Color gray = Color(0xFFD9D9D9);
+const Color secondaryAccentBackground = Color(0xFFE8C960);
+
+const Color accentFocus = Color(0xFFE2FFFA);
+
+const Color disabledBackground = Color(0xFFECECEC);
+const Color disabledForeground = Color(0xFF898989);
+
+const Color greenBackground = Color(0xFFE0FFE9);
+const Color greenForeground = Color(0xFF198038);
+
+const Color yellowBackground = Color(0xFFFFEFB9);
+const Color yellowForeground = Color(0xFF9C7900);
+
+const Color orangeBackground = Color(0xFFFFECDE);
+const Color orangeForeground = Color(0xFFDD5C00);
+
+const Color error = Color(0xFFD12831);
+const Color errorBackground = Color(0xFFFFDBDD);
+const Color errorDisabled = Color(0xFFAA898A);
+
+// Dimensions
+const double appbarPadding = 25;
+const double appbarHeight = 75;
