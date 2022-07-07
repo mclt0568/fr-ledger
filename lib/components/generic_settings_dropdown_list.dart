ffa5254bdab5f0635ce942d58da2bf3d15ac28d2
frankium <frankium>
2022-07-07 18:50:36 +0800

    add support for lists too long to display

--- lib/components/generic_settings_dropdown_list.dart
+++ lib/components/generic_settings_dropdown_list.dart
@@ -34,78 +34,118 @@ class GenericSettingsDropdownList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double availableSpace = screenHeight - y - pagePadding;
+    double calculatedHeight = (height * control.values.length) + height + 1;
+    if (calculatedHeight > availableSpace) {
+      calculatedHeight = availableSpace;
+    }
+
     return Container(
-        decoration: const BoxDecoration(
-            color: white, boxShadow: [slightElevatedOverOverlay]),
-        width: width,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            GestureDetector(
-                onTap: () {
-                  close();
-                },
-                child: Row(
-                  children: [
-                    SizedBox(
-                        width: height,
-                        height: height,
-                        child: Icon(icon,
-                            color: paragraphColorDark, size: standardIconSize)),
-                    Expanded(
-                      child: DefaultText(
-                        text: (control.values[control.selected] ??
-                                const DropdownListContent(
-                                    display: "", value: ""))
-                            .display,
-                      ),
-                    ),
-                    SizedBox(
-                        width: height,
-                        height: height,
-                        child: const Icon(CarbonIcons.chevron_up,
-                            color: paragraphColorDark, size: standardIconSize)),
-                  ],
-                )),
-            const Divider(height: 1),
-            for (String key in control.values.keys)
-              GestureDetector(
-                  onTap: () {
-                    bool toClose = onSelect?.call(key) ?? true;
-                    if (toClose) {
-                      close();
-                    }
-                  },
-                  child: Row(
+      decoration: const BoxDecoration(
+          color: white, boxShadow: [slightElevatedOverOverlay]),
+      width: width,
+      constraints: BoxConstraints(maxHeight: calculatedHeight),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          GestureDetector(
+            onTap: () {
+              close();
+            },
+            child: Row(
+              children: [
+                SizedBox(
+                    width: height,
+                    height: height,
+                    child: Icon(icon,
+                        color: paragraphColorDark, size: standardIconSize)),
+                Expanded(
+                  child: DefaultText(
+                    text: (control.values[control.selected] ??
+                            const DropdownListContent(display: "", value: ""))
+                        .display,
+                  ),
+                ),
+                SizedBox(
+                    width: height,
+                    height: height,
+                    child: const Icon(CarbonIcons.chevron_up,
+                        color: paragraphColorDark, size: standardIconSize)),
+              ],
+            ),
+          ),
+          const Divider(height: 1),
+          Container(
+            // color: grayBackground,
+            constraints:
+                BoxConstraints(maxHeight: calculatedHeight - height - 1),
+            child: MediaQuery.removePadding(
+              removeTop: true,
+              context: context,
+              child: Scrollbar(
+                child: SingleChildScrollView(
+                  physics: const BouncingScrollPhysics(),
+                  child: Column(
                     children: [
-                      SizedBox(
-                          width: height,
-                          height: height,
-                          child: (control.values[key] ??
-                                          const DropdownListContent(
-                                              display: "", value: ""))
-                                      .icon ==
-                                  null
-                              ? const SizedBox()
-                              : Icon(
-                                  (control.values[key] ??
-                                          const DropdownListContent(
-                                              display: "", value: ""))
-                                      .icon,
-                                  size: standardIconSize,
-                                  color: paragraphColorDark,
-                                )),
-                      Expanded(
-                        child: DefaultText(
-                          text: (control.values[key] ??
-                                  const DropdownListContent(
-                                      display: "", value: ""))
-                              .display,
+                      for (String key in control.values.keys)
+                        GestureDetector(
+                          onTap: () {
+                            bool toClose = onSelect?.call(key) ?? true;
+                            if (toClose) {
+                              close();
+                            }
+                          },
+                          child: SizedBox(
+                            height: height,
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: [
+                                Container(
+                                  color: white,
+                                  width: height,
+                                  height: height,
+                                  child: (control.values[key] ??
+                                                  const DropdownListContent(
+                                                      display: "", value: ""))
+                                              .icon ==
+                                          null
+                                      ? const SizedBox()
+                                      : Icon(
+                                          (control.values[key] ??
+                                                  const DropdownListContent(
+                                                      display: "", value: ""))
+                                              .icon,
+                                          size: standardIconSize,
+                                          color: paragraphColorDark,
+                                        ),
+                                ),
+                                Expanded(
+                                  child: Container(
+                                    color: white,
+                                    child: Align(
+                                      alignment: Alignment.centerLeft,
+                                      child: DefaultText(
+                                        text: (control.values[key] ??
+                                                const DropdownListContent(
+                                                    display: "", value: ""))
+                                            .display,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
-                      )
                     ],
-                  ))
-          ],
-        ));
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
